510420ad3ea18a70be0e03da6357a2b5b8cb0e1f
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2022-12-24 16:09:38 +0900

    ~ 5.4 Route States

--- react-master-base/src/routes/Coins.tsx
+++ react-master-base/src/routes/Coins.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
@@ -22,9 +23,10 @@ const Coin = styled.li`
     border-radius: 15px;
     margin-bottom: 10px;
     a {
+        display: flex;
+        align-items: center;
         padding: 20px;
         transition: color 0.2s ease-in;
-        display: block;
     }
     &:hover{
         a {
@@ -39,53 +41,60 @@ const Title = styled.h1`
     padding-bottom: 50px;
 `;
 
-const coins = [
-    {
-        id: "btc-bitcoin",
-        name: "Bitcoin",
-        symbol: "BTC",
-        rank: 1,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-    },
-    {
-        id: "eth-ethereum",
-        name: "Ethereum",
-        symbol: "ETH",
-        rank: 2,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-    },
-    {
-        id: "hex-hex",
-        name: "HEX",
-        symbol: "HEX",
-        rank: 3,
-        is_new: false,
-        is_active: true,
-        type: "token",
-    },
-];
+const Loader = styled.span`
+    text-align: center;
+    display: block;
+`;
+
+const Img = styled.img`
+    width: 40px;
+    height: 40px;
+    margin-right: 10px;
+`;
 
 interface CoinInterface {
-
-}
+    id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    is_new: boolean,
+    is_active: boolean,
+    type: string,
+};
 
 function Coins() {
-    return <Container>
-        <Header>
-            <Title>코인</Title>
-        </Header>
-        <CoinsList>
-            {coins.map(coin => (
-                <Coin key={coin.id}>
-                    <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link >
-                </Coin>
-            ))}
-        </CoinsList>
-    </Container>;
+    const [coins, setCoins] = useState<CoinInterface[]>([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        (async () => {
+            const response = await fetch("https://api.coinpaprika.com/v1/coins");
+            const json = await response.json();
+            setCoins(json.slice(0, 100));
+            setLoading(false);
+        })();
+    }, []);
+    return (
+        <Container>
+            <Header>
+                <Title>코인</Title>
+            </Header>
+            {loading ? <Loader>Loading...</Loader> : (
+                <CoinsList>
+                    {coins.map((coin) => (
+                        <Coin key={coin.id}>
+                            <Link to={{
+                                pathname: `/${coin.id}`,
+                                state: { name: coin.name },
+                            }}>
+                                <Img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} />
+                                {coin.name} &rarr;
+                            </Link >
+                        </Coin>
+                    ))}
+                </CoinsList>
+            )}
+        </Container>
+    );
 };
 
 export default Coins;
