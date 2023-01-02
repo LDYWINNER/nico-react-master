3ddaf4657874f86f6f6d53c6c27fdc6a6cdfe8eb
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-01-02 22:15:08 +0900

    ~6.15 Immutability

--- trello-clone/src/components/CreateToDo.tsx
+++ trello-clone/src/components/CreateToDo.tsx
@@ -1,5 +1,27 @@
+import { useForm } from "react-hook-form";
+import { useSetRecoilState } from "recoil";
+import { toDoState } from "./atoms";
+
+interface IForm {
+    toDo: string;
+};
+
 function CreateToDo() {
-    return null;
+    const setToDos = useSetRecoilState(toDoState);
+    const { register, handleSubmit, setValue } = useForm<IForm>();
+    const handleValid = ({ toDo }: IForm) => {
+        console.log("add to do", toDo);
+        setToDos((oldToDos) => [{ text: toDo, id: Date.now(), category: "To_DO" }, ...oldToDos])
+        setValue("toDo", "");
+    };
+    return (
+        <form onSubmit={handleSubmit(handleValid)}>
+            <input {...register("toDo", {
+                required: "Please write a todo",
+            })} placeholder="Write a to do" />
+            <button>Add</button>
+        </form>
+    );
 }
 
 export default CreateToDo;
