0894d4d3adfd601f807be85c297a8413e672d42a
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-01-03 00:25:27 +0900

    ~6.16 Selectors part One

--- trello-clone/src/components/ToDoList.tsx
+++ trello-clone/src/components/ToDoList.tsx
@@ -1,21 +1,36 @@
 import { useRecoilValue } from "recoil";
-import { toDoState } from "./atoms";
+import { toDoSelector, toDoState } from "./atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-    const toDos = useRecoilValue(toDoState);
-    console.log(toDos);
+    const [toDos, doings, dones] = useRecoilValue(toDoSelector);
     return (
         <div>
             <h1>To Dos</h1>
             <hr />
             <CreateToDo />
+            <h2>To Do</h2>
             <ul>
                 {toDos.map((toDo) => (
                     <ToDo key={toDo.id} {...toDo} />
                 ))}
             </ul>
+            <hr />
+            <h2>Doing</h2>
+            <ul>
+                {doings.map((doing) => (
+                    <ToDo key={doing.id} {...doing} />
+                ))}
+            </ul>
+            <hr />
+            <h2>Done</h2>
+            <ul>
+                {dones.map((done) => (
+                    <ToDo key={done.id} {...done} />
+                ))}
+            </ul>
+            <hr />
         </div>
     );
 };
